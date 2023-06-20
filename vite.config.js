38801d205ecaa514edc8921da20ac87a55966935
Raman Davidovich <raman.davidovich@gmail.com>
2023-06-20 12:15:50 +0300

    change: changed vite config to bundle analise

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import analize from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), analize.default()],
+});
